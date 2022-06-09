ea21f5476e3248e1e8847e3a49b1a9b4127d6d34
Parker Bedlan <parkerbedlan@gmail.com>
2022-06-08 19:11:03 -0500

    works for local production server

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 // server.js
 import http from 'http';
 import { handler } from './build/handler.js'; // <- Import SvelteKit handlers
+import injectSocketIO from './socket-handler.js'; // The SocketIO stuff (see next step)
 import express from 'express';
 
 const app = express();
 const server = http.createServer(app);
+
+// Inject SocketIO
+injectSocketIO(server);
 
 app.set('proxy', 1);
 
